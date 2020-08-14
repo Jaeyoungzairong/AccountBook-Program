2a2d30f1f5d42550ac7f794e426d1b63ff77e983
Jaeyoung <69396761+Jaeyoungzairong@users.noreply.github.com>
2020-08-14 18:06:46 +0800

    Add files via upload

---
+++ GV.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountBookProgram
+{
+    public class GV
+    {
+        public static DispayStatus UpdateDisplayStatus = DispayStatus.Account; //화면상태
+        public static SaveStatus SaveMode = SaveStatus.None; //Data 저장상태
+    }
+}
---
+++ GeneralType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountBookProgram
+{
+    public enum DispayStatus
+    {
+        None,
+        Account,
+        AccountSetting,
+        Book,
+        BookSetting,
+        Config
+    }
+    public enum SaveStatus
+    {
+        None,
+        Add,
+        Modify,
+        Delete
+    }
+}
